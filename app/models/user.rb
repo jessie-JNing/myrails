1af35dadd9b5fcd505ed87afa867eaea7c5caa87
jessie ning <jessie.ning@shopify.com>
2019-03-31 19:13:06 -0400

    add login

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,13 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+               BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
   has_many :microposts
   # validates FILL_IN, presence: true    # Replace FILL_IN with the right code.
 end
